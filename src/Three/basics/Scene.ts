d093414b223c88d734fa6634e1ef9fe1d842d8de
Kevin-Leguiza-Gaggero <nicolaskevinleguiza@gmail.com>
2022-07-22 03:32:30 +0000

    Add 'getTextureFromUrl' method

--- src/Three/basics/Scene.ts
+++ src/Three/basics/Scene.ts
@@ -1,14 +1,24 @@
 import * as THREE from "three";
 
+export const getTextureFromUrl = (path: string, callback?: (texture: THREE.Texture) => void | undefined): Promise<THREE.Texture> => {
+  return new Promise((resolve) => {
+    const loader = new THREE.TextureLoader();
+    loader.loadAsync(path).then((texture) => {
+      if (callback) callback(texture);
+      resolve(texture);
+    });
+  });
+};
+
 interface Props {
   background?: THREE.Color | THREE.Texture;
   meshes?: THREE.Object3D<THREE.Event>[] | THREE.Object3D<THREE.Event>;
   // (THREE.Mesh | THREE.Line)[] | THREE.Mesh | THREE.Line;
 }
-
-export const createScene = ({ background = new THREE.Color("#333"), meshes }: Props) => {
+export const createScene = (sceneProps: Props = {}) => {
+  const { background, meshes } = sceneProps ?? { background: new THREE.Color("#333"), meshes: [] };
   const scene = new THREE.Scene();
-  scene.background = background;
+  scene.background = background ?? new THREE.Color("#333");
 
   if (meshes) {
     if (Array.isArray(meshes)) for (const mesh of meshes) scene.add(mesh);
